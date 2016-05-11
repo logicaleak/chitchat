bad982fb5d200be0a4ffc30401af2b1b65d857c3
ozum safaoglu <ozumsafaoglu@yandex.com>
2016-05-12 01:49:31 +0300

    On click action bar

--- js/component/feed/FeedElement.js
+++ js/component/feed/FeedElement.js
@@ -4,38 +4,83 @@ import ImageProvider from '../../util/ImageProvider'
 export default class FeedElement extends React.Component {
     constructor(props) {
         super(props)
+        
+        this._renderActionBar = this._renderActionBar.bind(this);
+        
+        this.state = {
+            actionBarVisible : false
+        };
     }
     
     _onPressButton() {
+        console.log("pressed");
+        if (this.state.actionBarVisible) {
+            this.setState({
+                actionBarVisible : false 
+            });    
+        } else {
+            this.setState({
+                actionBarVisible : true 
+            });
+        }
+        
+    }
+    
+    _renderActionBar() {
+        console.log("in");
+        if (this.state.actionBarVisible) {
+            console.log("true");
+            return (
+                
+                <View style={styles.actionBar}>
+                    <TouchableNativeFeedback
+                            background={TouchableNativeFeedback.SelectableBackground()} >
+                        <View style={styles.engageButton}>
+                            <Text>Engage</Text>
+                        </View>
+                    </TouchableNativeFeedback>
+                    <TouchableNativeFeedback
+                            background={TouchableNativeFeedback.SelectableBackground()} >
+                        <View style={styles.dismissButton}>
+                            <Text>Dismiss</Text>
+                        </View>
+                    </TouchableNativeFeedback>
+                </View>
+                
+            );        
+        } 
     }
     
     render() {
         var ozu = ImageProvider.getOzu();
         var feedElementData = this.props.feedElementData;
         return (
-            <TouchableNativeFeedback
-                    onPress={this._onPressButton.bind(this)}
-                    background={TouchableNativeFeedback.SelectableBackground()} >
-                <View style={styles.container}>
-                    <View style={styles.metaData}>
-                        <View style={styles.name}>
-                            <Text style={styles.boldText}>{feedElementData.owner.userNameSurname}</Text>
+            <View>
+                <TouchableNativeFeedback
+                        onPress={this._onPressButton.bind(this)}
+                        background={TouchableNativeFeedback.SelectableBackground()} >
+                    <View style={styles.container}>
+                        <View style={styles.metaData}>
+                            <View style={styles.name}>
+                                <Text style={styles.boldText}>{feedElementData.owner.userNameSurname}</Text>
+                            </View>
+                            <View style={styles.topic}>
+                                <Text style={styles.boldText}>  {feedElementData.topicName}</Text>
+                            </View>
                         </View>
-                        <View style={styles.topic}>
-                            <Text style={styles.boldText}>  {feedElementData.topicName}</Text>
+                        <View style={styles.separator} />
+                        <View style={styles.bottomContent}>
+                            <View style={styles.mainContent}>
+                                <Text key={feedElementData.feedId} >
+                                    {feedElementData.userMessage}
+                                </Text>
+                            </View>
                         </View>
+                        
                     </View>
-                    <View style={styles.separator} />
-                    <View style={styles.bottomContent}>
-                        <View style={styles.mainContent}>
-                            <Text key={feedElementData.feedId} >
-                                {feedElementData.userMessage}
-                            </Text>
-                        </View>
-                    </View>
-                </View>
-            </TouchableNativeFeedback>
-             
+                </TouchableNativeFeedback>
+                {this._renderActionBar()}
+            </View>
         );
     }
 }
@@ -73,5 +118,18 @@ const styles = StyleSheet.create({
     separator: {
         height: 0.3,
         backgroundColor : '#808080'
-    }
+    },
+    actionBar : {
+        flexDirection : 'row',
+        height : 15
+    },
+    engageButton : {
+        flex: 1,
+        backgroundColor: 'green'
+    },
+    dismissButton: {
+        flex: 1,
+        backgroundColor : 'red'
+    } 
+    
 });
